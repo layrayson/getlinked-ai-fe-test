f43c81a122930d087b511cd927f84db385bb61b2
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-06-04 19:31:33 +0100

    FEAT: implement header

---
+++ src/components/shared/Section.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+  className?: string;
+};
+export const Section = ({ children, className = "" }: Props) => {
+  return <div className={"max-w-1149 mx-auto " + className}>{children}</div>;
+};
