4b66cdc136b2340ece2296cf4cd0399524c38318
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-06-05 20:08:46 +0100

    FEAT: add get linked AI

--- src/app/(homepage)/layout.tsx
+++ src/app/(homepage)/layout.tsx
@@ -8,7 +8,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div>
       <div>
-        <div className="fixed top-0 w-full">
+        <div className="fixed top-0 w-full z-10">
           <Header />
         </div>
         <div className="h-98px" />
--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,4 +1,5 @@
 import SystemCheck from "@/components/home/SystemCheck";
+import { Section } from "@/components/shared/Section";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,10 +8,20 @@ export const metadata: Metadata = {
 };
 const Home = () => {
   return (
-    <div>
+    <div className="h-screen relative">
       <div className="mt-6">
         <SystemCheck />
       </div>
+      <Section className="mt-112px mb-66px">
+        <div className="flex gap-x-2 items-center  ">
+          <div>
+            <p className="text-blueGray-300 text-sm">POWERED BY</p>
+          </div>
+          <div>
+            <p className="text-blue-900 font-medium text-lg">GetLinked.AI</p>
+          </div>
+        </div>
+      </Section>
     </div>
   );
 };
